eb8313643f56a4926df29cdb735a08ad3832ddfe
juliagangi <97065262+juliagangi@users.noreply.github.com>
2023-01-30 23:42:30 -0600

    q

--- awardnames.py
+++ awardnames.py
@@ -108,8 +108,6 @@ def worst_dressed(data):
                         seen[new_selection] = seen[new_selection]+1
                     else:
                         seen[new_selection]=1
-    #print(seen)
-    #exit()
     for item in seen:
         curr_worst = seen[item]
         curr_worst_dressed = item
@@ -136,7 +134,24 @@ def extract_tweets(data):
                 break
     return tweetarr
 
-def find_awards(tweets):
+'''
+def extract_tweets(data):
+    tweetarr = []
+    keywords = ['wins', 'won', 'win', 'named', 'nominated', 'nominee', 'award', 'goes to', 'up for']
+    for element in data:
+        tweet = element['text']
+        if tweet.split()[0] == 'RT':
+            continue
+        for keyword in keywords:
+            tweet = tweet.lower()
+            if tweet.__contains__(keyword):
+                if tweet.__contains__('best'):
+                    tweetarr.append(tweet)
+                break
+    return tweetarr
+'''
+
+def find_awards(data):
     award_names = []
     prohibited_punctuation = ['.','?','!']
     prohibited_elements = ['#','http']
@@ -144,7 +159,11 @@ def find_awards(tweets):
     prohibited_POS = ["ADP","AUX","CONJ","DET","PRON","SCONJ"]
     before_keywords = ['award for', 'wins for','wins','won','named','win','nominated for','up for']
     after_keywords = ['goes to','went to']
-    for origtweet in tweets:
+    for element in data:
+        origtweet = element['text']
+        #if origtweet.split()[0] == 'RT':
+        #    continue
+        origtweet = origtweet.lower()
         curr_list = {}
         ind = 0 
         tweet = origtweet.split()
@@ -172,9 +191,11 @@ def find_awards(tweets):
                         continue
                     if nlp(lastword)[0].pos_ != "NOUN":
                         continue
-                    portion = ' '.join(portion)
                     if not portion.__contains__('best'):
-                        continue        
+                        continue
+                    if portion.__contains__('dressed') or portion.__contains__('speech'):
+                        continue 
+                    portion = ' '.join(portion)      
                     for symbol in prohibited_punctuation:
                         if lastword[len(lastword)-1] == symbol:
                             portion = portion[0:len(portion)-1]
@@ -186,10 +207,10 @@ def find_awards(tweets):
                             breaking = 1  
                     if breaking:
                         break
-                    #if nlp(lastword[len(lastword)-1])[0].pos_ == "PUNCT":
-                    #    portion = portion[0:len(portion)-1]
-                        # cut off punctuation
-                        # don't want to standardize incorrect phrases
+                    if nlp(lastword[len(lastword)-1])[0].pos_ == "PUNCT":
+                        portion = portion[0:len(portion)-1]
+                        #cut off punctuation
+                        #don't want to standardize incorrect phrases
                     curr_list[portion] = 1
                 award_names = award_names + [curr_list]
                 break
@@ -214,15 +235,17 @@ def find_awards(tweets):
                     continuing = 1
                     if nlp(firstword)[0].pos_ != "ADJ":
                         continue
-                    portion = ' '.join(portion)
                     if not portion.__contains__('best'):
                         continue 
-                     #check leftmost word isn't in prev sentence
+                    if portion.__contains__('dressed') or portion.__contains__('speech'):
+                        continue 
+                    portion = ' '.join(portion)
+                    #check leftmost word isn't in prev sentence
                     for symbol in prohibited_punctuation:
                         if firstword[len(firstword)-1] == symbol:
                             breaking = 1
-                    #if nlp(lastword[len(lastword)-1])[0].pos_ == "PUNCT":
-                        #portion = portion[0:len(portion)-1]
+                    if nlp(lastword[len(lastword)-1])[0].pos_ == "PUNCT":
+                        portion = portion[0:len(portion)-1]
                         #cut off punctuation
                         #don't need to cut off end punct?
                     for symbol in prohibited_elements:
@@ -252,8 +275,12 @@ def rank_awards(awards):
         if len(list2)==0:
             continue
         most_freq_pair = get_max_freq(list2)
+        if most_freq_pair[0] in updated_seen:
+            updated_seen[most_freq_pair[0]] = updated_seen[most_freq_pair[0]] + most_freq_pair[1]
         if most_freq_pair[0] not in updated_seen:
             updated_seen[most_freq_pair[0]] = most_freq_pair[1]
+
+    '''
     # combine different namings of same award
     for award1 in updated_seen:
         for award2 in updated_seen:
@@ -277,25 +304,53 @@ def rank_awards(awards):
         if final_seen[award][1] > 1:
             most_frequent.append(award[0].append(award))
     return most_frequent
-
-           
     '''
+    # compare parts of speech:
+    for award in updated_seen:
+        nouns = []
+        adjs = []
+        for word in award.split():
+            if nlp(word)[0].pos_ == 'PUNCT':
+                continue
+            if nlp(word[len(word)-1])[0].pos_ == 'PUNCT':
+                word = word[0:len(word)-1]
+            if nlp(word)[0].pos_ == 'NOUN':
+                nouns.append(word)
+            elif nlp(word)[0].pos_ == 'ADJ':
+                adjs.append(word)
+        updated_seen[award] = [updated_seen[award],[nouns],[adjs],[]]
+    for award1 in updated_seen:
+        for award2 in updated_seen:
+            if award1 != award2:
+                if updated_seen[award1] != 0 and updated_seen[award2] != 0:
+                    nouns1 = updated_seen[award1][1]
+                    nouns2 = updated_seen[award2][1]
+                    adjs1 = updated_seen[award1][2]
+                    adjs2 = updated_seen[award2][2]
+                    if sorted(nouns1) == sorted(nouns2):
+                        if sorted(adjs1) == sorted(adjs2):
+                            updated_seen[award1] = [updated_seen[award1][0]+updated_seen[award2][0],updated_seen[award1][1],updated_seen[award1][2],updated_seen[award1][3]+[award2]]
+                            updated_seen[award2] = 0
+    final_seen = {}
+    for award in updated_seen:
+        if updated_seen[award] != 0:
+            final_seen[award] = [updated_seen[award][0],updated_seen[award][3]]
+
     most_frequent = {}
     i = 0
     for award in final_seen:
         # if updated_seen[award][1] > thres: most_frequent.append(award[0].append(award))
         if i < 25:
-            most_frequent[award] = updated_seen[award]
+            most_frequent[award] = final_seen[award]
             i = i + 1
             continue
-        curr_freq = updated_seen[award]
+        curr_freq = final_seen[award][0]
         min_pair = get_min_freq(most_frequent)
         min_freq = min_pair[1]
         if curr_freq > min_freq:
             min_freq_award = min_pair[0]
             most_frequent.pop(min_freq_award)
-            most_frequent[award] = curr_freq
-    '''
+            most_frequent[award] = final_seen[award]
     return most_frequent
 
 def get_min_in_list(list): # list of 2-item lists
@@ -310,11 +365,11 @@ def get_min_in_list(list): # list of 2-item lists
 
 def get_min_freq(dict):
     for key in dict:
-        curr_min = dict[key]
+        curr_min = dict[key][0]
         curr_min_award = key
         break
     for key in dict:
-        val = dict[key]
+        val = dict[key][0]
         if val < curr_min:
             curr_min = val
             curr_min_award = key
@@ -332,13 +387,13 @@ def get_max_freq(dict):
             curr_max_award = key
     return (curr_max_award, curr_max)
 
-
 def get_nominees(award):
     return ["here","heree"]
 
 def get_awards():
-    tweetarr = extract_tweets(data)
-    awards = find_awards(tweetarr)
-    rank_awards(awards)
+    awards = find_awards(data)
+    print(rank_awards(awards))
+
+get_awards()
 
 
